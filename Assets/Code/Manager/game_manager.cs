b722a41d9fb4b11333f6d7c13e3b8b9234e9379d
13985 <1155158931@link.cuhk.edu.hk>
2023-02-20 20:56:27 +0800

    generate node
    node used in pathfinding

---
+++ Assets/Code/Manager/game_manager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class game_manager : MonoBehaviour{
+    public readonly int UP=0,DOWN=3,LEFT=1,RIGHT=2;
+    public int row,column;
+
+
+    private int lives,scores;
+    void Start(){
+        
+    }
+
+    void Update(){
+        
+    }
+}
