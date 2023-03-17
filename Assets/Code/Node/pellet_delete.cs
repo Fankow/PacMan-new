c0d7400591644ba9231855e70d2519525c8e325b
Name <125875608+13985@users.noreply.github.com>
2023-03-18 00:46:41 +0800

    Update pellet_delete.cs

--- Assets/Code/Node/pellet_delete.cs
+++ Assets/Code/Node/pellet_delete.cs
@@ -4,13 +4,9 @@ using UnityEngine;
 
 public class pellet_delete : MonoBehaviour{
     void Start(){
-        StartCoroutine(SelfDestroy());
+        Destroy(gameObject,2f);
     }
-    IEnumerator SelfDestroy(){
-        yield return new WaitForSeconds(2);
-        Destroy(gameObject);
-        yield break;
-    }
+
 
     void OnTriggerEnter2D(Collider2D other){
         if(other.gameObject.CompareTag("node_pellet")){
